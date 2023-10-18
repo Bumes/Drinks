a1a7b8fe2663582437fc406ca9360fe0229002d4
Lukas Bumes <lukas.bumes@st.oth-regensburg.de>
2023-10-18 16:36:53 +0200

    modified:   drinks.js

--- drinks.js
+++ drinks.js
@@ -14,7 +14,7 @@ async function fetchAndStoreIngredients() {
 }
   
 function fetchIngredients() {
-    return fetch('https://raw.githubusercontent.com/Bumes/Drinks/main/available-ingredients.json')
+    return fetch('https://raw.githubusercontent.com/Bumes/Drinks/main/available-ingredients.json?v=' + new Date().getTime())
         .then(response => {
         if (!response.ok) {
             throw new Error('Network response was not ok');
@@ -24,6 +24,8 @@ function fetchIngredients() {
 }
 
 // #endregion
+
+
 
 function Drink(name, picture, ingredients, garnish) {
     
