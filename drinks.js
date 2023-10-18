0e7808a77184eccad2670c629eabbc29eb8bb518
Lukas Bumes <lukas.bumes@st.oth-regensburg.de>
2023-10-18 15:14:53 +0200

    modified:   drinks.js

--- drinks.js
+++ drinks.js
@@ -1,4 +1,52 @@
+
+const avaible_ingredients= {
+    "beer": true,
+
+    "vodka": true,
+    "gin": true,
+    "white_rum": true,
+    "brown_rum": true,
+
+    "lillet": true,
+    "aperol": true,
+    "dry_vermouth": false,
+    "secco": true,
+
+    "sparkling_water": true,
+    "wild_berry": true,
+    "ginger_ale": true,
+    "ginger_beer": false,
+    "coca_cola": true,
+    
+    "lime": true,
+    "lime_juice": true,
+    "mint_leaves": true,
+    "orange": false,
+    "brown_sugar": true
+};
+
+/*function processJSON(data) {
+    const available_ingredients = data;
+}
+
+fetch('available-ingredients.json')
+    .then(response => response.json())
+    .then(processJSON) // Call the function with the JSON data
+    .catch(error => {
+        console.error('Error reading the JSON file:', error);
+    }
+);*/
+
 function Drink(name, picture, ingredients, garnish) {
+    
+    for (ingredient of ingredients) {
+        formatted_ingredient = ingredient.toLowerCase().replace(/[\d½]+(ml|g)? /, '').replace(/ /g, '_')
+        if (!avaible_ingredients[formatted_ingredient]){
+            console.log(formatted_ingredient + ": " + avaible_ingredients[formatted_ingredient])
+            return;
+        }
+    }
+
     const menu = document.getElementById("menu");
 
     // Create a drink container
@@ -6,7 +54,6 @@ function Drink(name, picture, ingredients, garnish) {
     drinkDiv.classList.add("drink");
 
     const horizontal = window.innerHeight < window.innerWidth ? "-horizontal" : "-portrait";
-    console.log("image-area"+horizontal)
 
     // Populate the drink container
     drinkDiv.innerHTML = `
@@ -70,7 +117,7 @@ function Drink(name, picture, ingredients, garnish) {
 }
 
 
-Drink("Mojito", "mojito.jpg", ["60ml White Rum", "15g Brown Sugar", "½ Lime", "Fresh Mint Leaves", "Sparkling Water"], "Mint Leaves");
+Drink("Mojito", "mojito.jpg", ["60ml White Rum", "15g Brown Sugar", "½ Lime", "Mint Leaves", "Sparkling Water"], "Mint Leaves");
 Drink("Cuba Libre", "cuba-libre.png", ["60ml Brown Rum", "½ Lime", "Coca Cola"], "Lime");
 Drink("Aperol Spritz", "aperol-spritz.png", ["60ml Secco", "30ml Aperol", "Sparkling Water"], "Orange")
 Drink("Martini", "martini.jpg", ["60ml Gin", "15ml Dry Vermouth"], "Lemon Twist or Olives");
