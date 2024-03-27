e89cd2fe8f6733253080875e6d3cb99f8948c066
Lukas Bumes <lukas.bumes@st.oth-regensburg.de>
2024-03-27 10:20:45 +0100

    modified:   index.html new file:   language.js modified:   master.html modified:   script.js

--- script.js
+++ script.js
@@ -841,36 +841,6 @@ async function fetchAndStoreLanguages() {
 
 let language;
 
-function update_language() {
-    let userLanguage = navigator.language || navigator.userLanguage;
-    userLanguage = userLanguage.split("-")[0]
-    console.log("Set Language to:")
-    console.log(userLanguage)
-    fetchAndStoreLanguages()
-        .then(languages => {
-            // languages will now contain the actual data after successful fetch
-            if (!languages.hasOwnProperty(userLanguage)) {
-                userLanguage = "en"
-            }
-            language = languages[userLanguage];
-            console.log(language);
-            for (const key in language) {
-                if (typeof(language[key]) != "object") {
-                    const elements = document.querySelectorAll(`#${key}`); // Use querySelectorAll to get all elements
-
-                    if (elements.length > 0) {
-                        for (const element of elements) {
-                            element.textContent = language[key];
-                        }
-                    } else {
-                        console.info(`Element with ID "${key}" not found.`);
-                    }
-                }
-            }
-        })
-        .catch(error => console.error(error));  // Handle errors from fetchLanguages
-}
-
 function sendData() {
     var inputData = document.getElementById("inputData").value;
     var xhr = new XMLHttpRequest();
@@ -886,5 +856,6 @@ function sendData() {
         console.error('Error while sending data.');
     };
     var data = JSON.stringify({ input: inputData });
+    console.log("Sending data: " + data)
     xhr.send(data);
 }
